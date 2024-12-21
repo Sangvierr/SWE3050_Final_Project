24ae3073897ac6eaaaeddebf413713634e00faf7
leesanghyuk2000 <leesanghyuk2000@gmail.com>
2024-12-22 00:15:35 +0900

    Chore: add comments about complexity

--- kernel.py
+++ kernel.py
@@ -9,3 +9,7 @@ def weighted_jaccard_kernel(X1, X2):
             max_sum = np.sum(np.maximum(vec1, vec2))
             kernel_matrix[i, j] = min_sum / max_sum if max_sum != 0 else 0
     return kernel_matrix
+
+# 위 커널의 경우 복잡도가 O(N X M X D)
+# N: X1의 행 개수, M: X2의 행 개수, D: X1, X2의 열의 개수
+# 따라서 N, M, D가 커질수록 계산량이 과도하게 많아짐
